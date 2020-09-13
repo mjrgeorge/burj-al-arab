be02427ad1565429c79a7b4b0d76ee1f596db482
mjrgeorge <mjrgeorge@gmail.com>
2020-09-13 18:34:53 +0600

    login added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkS4b4QxLyquCjUM5dDRbKA0pw_nNZmt0",
+    authDomain: "burj-al-arab-b9fea.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-b9fea.firebaseio.com",
+    projectId: "burj-al-arab-b9fea",
+    storageBucket: "burj-al-arab-b9fea.appspot.com",
+    messagingSenderId: "104960192463",
+    appId: "1:104960192463:web:30f31673d3988be1122cba"
+  };
+  
+  export default firebaseConfig;
