f59083e5bc1a81cd804d2e10e66b450e41707ba7
mjrgeorge <mjrgeorge@gmail.com>
2020-09-29 08:38:47 +0600

    book component update

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
 
 const Book = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const {bedType} = useParams();
     return (
         <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
+            <h1>{loggedInUser.name}! Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
         </div>
     );
